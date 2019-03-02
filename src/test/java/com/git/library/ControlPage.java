c12a1f6ba5436417cd37e48cd84ea4b6f01acf48
Nabin Gyawaly <Sanu Subedi@DESKTOP-FMGC15H.fios-router.home>
2019-03-02 01:36:59 -0500

    Adding @Test

--- src/test/java/com/git/library/ControlPage.java
+++ src/test/java/com/git/library/ControlPage.java
@@ -3,7 +3,6 @@ package com.git.library;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
--- src/test/java/com/git/tests/TestPage.java
+++ src/test/java/com/git/tests/TestPage.java
@@ -1,11 +1,14 @@
 package com.git.tests;
 
+import org.testng.annotations.Test;
+
 import com.git.library.ControlPage;
 import com.git.pages.HomePage;
 
 public class TestPage extends ControlPage {
 	HomePage myHomePage = new HomePage();
 
+	@Test
 	public void testpage() {
 		myHomePage.getWebsite();
 	}
